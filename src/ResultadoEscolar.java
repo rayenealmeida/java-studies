c5c026f64c1dc72ef89ed5a3623f93fe4d44aa4b
almeidarayene <almeidarayene@gmail.com>
2023-05-30 17:38:02 -0300

    aprovado

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolar {
     
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 8;
 
         if(nota >=7){
             System.out.println("Aprovado");
