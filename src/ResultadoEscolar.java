05e491a5d278f9ef572eb85cffbb81932850f2f0
almeidarayene <almeidarayene@gmail.com>
2023-05-30 17:45:26 -0300

    condicional encadeada

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,11 +1,16 @@
 public class ResultadoEscolar {
     
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 5;
 
         if(nota >=7){
             System.out.println("Aprovado");
         }
+        //condicional encadeada
+        else if (nota >= 5 && nota < 7 ){
+            System.out.println("Prova Recuperação");
+        }
+
         else {
             System.out.println("Reprovado");
         }
--- src/caixaEletronico.java
+++ src/caixaEletronico.java
@@ -1,10 +1,15 @@
 public class caixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 22.0;
 
         if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }
+       
+        else{
+            System.out.println("Saldo Insuficiente");
         }
 
         System.out.print(saldo);
